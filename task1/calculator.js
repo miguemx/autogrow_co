71cb74d60d8e1e665b73e688b0c026880119c443
miguemx <mxmigue@gmail.com>
2024-05-09 00:53:56 -0600

    confused about calculator

--- task1/calculator.js
+++ task1/calculator.js
@@ -19,4 +19,48 @@ function calculate() {
         document.getElementById('display').value = 'Error';
     }
 }
+
+
+
+const slider1 = document.getElementById("slider1");
+const slider2 = document.getElementById("slider2");
+const slider3 = document.getElementById("slider3");
+const totalOutput = document.getElementById("totalValue");
+const appointment = document.getElementById("appointment");
+const returns = document.getElementById("return");
+
+// Function to calculate and update the total value
+function updateTotal() {
+  const total = parseInt(slider1.value) + parseInt(slider2.value) + parseInt(slider3.value);
+  const app = Math.floor (total / 9);
+  const ret = Math.ceil(total*2.5);
+  totalOutput.innerHTML = "$ " + total;
+  appointment.innerHTML = app;
+  returns.innerHTML = ret + " %";
+}
+
+// Initialize total value
+updateTotal();
+
+
+
+// 
+
+document.getElementById("slider1").oninput = function() {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #FBC530 0%, #FBC530 ' + value + '%, #dedede ' + value + '%, #dedede 100%)';
+    updateTotal();
+};
+
+document.getElementById("slider2").oninput = function() {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #FBC530 0%, #FBC530 ' + value + '%, #dedede ' + value + '%, #dedede 100%)';
+    updateTotal();
+};
+
+document.getElementById("slider3").oninput = function() {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #FBC530 0%, #FBC530 ' + value + '%, #dedede ' + value + '%, #dedede 100%)';
+    updateTotal();
+};
   
